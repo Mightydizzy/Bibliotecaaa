2f3f71153681f5415a52dc99088c692e1d321e13
valeria <valerialucia1998@gmail.com>
2024-12-07 23:37:09 -0300

    commit

--- src/models/class_client.py
+++ src/models/class_client.py
@@ -1,5 +1,3 @@
-from src.utils.hashing import hash_password
-import re
 class Client:
     def __init__(self, id_cliente=None, nombre= None, email=None, password=None):
         self.id_cliente = id_cliente
